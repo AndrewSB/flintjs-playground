fa5037571b9e20526c3626ea01643b70d7d73afe
Andrew Breckenridge <asbreckenridge@me.com>
2015-10-30 16:22:32 -0700

    init with venn

---
+++ venn/main.js
@@ -0,0 +1,22 @@
+view Main {
+  <Circle class="blue" />
+
+  $ = { margin: 'auto' }
+  $blue = { background: 'blue' }
+}
+
+view Lol {
+  
+}
+
+view Circle {
+
+  $ = {
+    background: 'red',
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    opacity: 0.5,
+    margin: ['auto', 150],
+  }
+}
